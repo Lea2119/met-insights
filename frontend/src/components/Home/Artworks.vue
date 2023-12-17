62a8012bafecfb91d4dbb5ed26202bf2292f3851
Lea Cassegrain <lea.csgn@gmail.com>
2023-12-17 14:15:54 +0100

    Home : page created + Artworks section created

---
+++ frontend/src/components/Home/Artworks.vue
@@ -0,0 +1,71 @@
+<template>
+	<div class="artworks-container">
+		<div class="artworks-header w-100 pa-10">
+			<p class="text-h4 font-weight-black">ARTWORKS OF THE DAY</p>
+		</div>
+		<div class="d-flex justify-space-around pa-10">
+			<v-card max-width="550px" variant="flat" class="rounded-0">
+				<v-img
+					src="https://d3d00swyhr67nd.cloudfront.net/_source/artuk_stories/vincent-van-gogh-1853-1890-self-portrait-1887-the-art-institute-of-chicago-joseph-winterbotham-collection-725px-1.jpg"
+					height="250px"
+					cover
+				></v-img>
+				<div class="v-card-body py-5">
+					<div class="d-flex justify-start pa-0 mb-4">
+						<v-card-title class="pa-0 mr-4 font-weight-bold"
+							>Vincent Van Gogh</v-card-title
+						>
+						<v-card-title class="pa-0 font-weight-bold">
+							Portrait in Arles
+						</v-card-title>
+					</div>
+					<div>
+						<v-card-text class="card-title font-weight-medium text-justify pa-0 mb-4">
+							I'm a thing. But, like most politicians, he promised more than he could
+							deliver. You won't have time for sleeping, soldier, not with all the bed
+							making you'll be doing. Then we'll go with that data file! Hey, you add
+							a one and two zeros to that or we walk! You're going to do his laundry?
+							I've got to find a way to escape.
+						</v-card-text>
+					</div>
+					<div>
+						<v-btn class="w-50" variant="flat" color="black">MORE DETAILS</v-btn>
+					</div>
+				</div>
+			</v-card>
+			<v-card max-width="550px" variant="flat" class="rounded-0">
+				<v-img
+					src="https://d3d00swyhr67nd.cloudfront.net/_source/artuk_stories/vincent-van-gogh-1853-1890-self-portrait-1887-the-art-institute-of-chicago-joseph-winterbotham-collection-725px-1.jpg"
+					height="250px"
+					cover
+				></v-img>
+				<div class="v-card-body py-5">
+					<div class="d-flex justify-start pa-0 mb-4">
+						<v-card-title class="pa-0 mr-4 font-weight-bold"
+							>Vincent Van Gogh</v-card-title
+						>
+						<v-card-title class="pa-0 font-weight-bold">
+							Portrait in Arles
+						</v-card-title>
+					</div>
+					<div>
+						<v-card-text class="card-title font-weight-medium text-justify pa-0 mb-4">
+							I'm a thing. But, like most politicians, he promised more than he could
+							deliver. You won't have time for sleeping, soldier, not with all the bed
+							making you'll be doing. Then we'll go with that data file! Hey, you add
+							a one and two zeros to that or we walk! You're going to do his laundry?
+							I've got to find a way to escape.
+						</v-card-text>
+					</div>
+					<div>
+						<v-btn class="w-50" variant="flat" color="black">MORE DETAILS</v-btn>
+					</div>
+				</div>
+			</v-card>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
